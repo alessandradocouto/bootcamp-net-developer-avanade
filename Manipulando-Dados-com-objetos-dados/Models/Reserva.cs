cbffd808708ae5b7caaeba178be206b90ea22214
alessandradocouto <alessandradocouto@outlook.com>
2024-06-02 23:19:44 -0300

    add Desafio de Hospedagem Hotel C#

---
+++ Manipulando-Dados-com-objetos-dados/Models/Reserva.cs
@@ -0,0 +1,56 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; } = new List<Pessoa>();
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            // Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            if (Suite != null && Suite.Capacidade >= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                // Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+                throw new Exception("A capacidade da suite é menor que o número de hóspedes.");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            // Retorna a quantidade de hóspedes (propriedade Hospedes)
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // Retorna o valor da diária
+            // Cálculo: DiasReservados X Suite.ValorDiaria
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            if (DiasReservados >= 10)
+            {
+                valor *= 0.9m; // Conceder um desconto de 10%
+            }
+
+            return valor;
+        }
+    }
+}
